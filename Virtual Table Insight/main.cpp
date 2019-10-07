5418fbb537ea30a84be38dc4e034372743c90f57
Clément Dif <clement.dif@etu.isima.fr>
2019-10-07 22:42:32 +0200

    :art: Refactored the code about the vtables.

--- Virtual Table Insight/main.cpp
+++ Virtual Table Insight/main.cpp
@@ -1,35 +1,25 @@
-#include <iostream>
 #include <cstdlib>
+#include <iostream>
 #include <string>
 
 class Entity
 {
-public:
-	virtual std::string getName()
-	{
-		return "The entity has no name";
-	}
+  public:
+	virtual std::string getName() { return "The entity has no name"; }
 };
 
 class Person : public Entity
 {
-private:
+  private:
 	std::string m_name;
 
-public:
-	Person(std::string name)
-		:m_name(name) {}
+  public:
+	Person(std::string name) : m_name(name) {}
 
-	std::string getName() override
-	{
-		return m_name;
-	}
+	std::string getName() override { return m_name; }
 };
 
-void printName(Entity& e)
-{
-	std::cout << e.getName() << std::endl;
-}
+void printName(Entity &e) { std::cout << e.getName() << std::endl; }
 
 int main(int, char **)
 {
